78f636e9e10d9a0f6b9cf998205ef788446ef34d
JiiXaa <anythingfx@gmail.com>
2022-02-24 22:26:01 +0100

    added component structure and some styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import {
+  About,
+  Footer,
+  Header,
+  Projects,
+  Skills,
+  Testimonials,
+} from './container';
+
+import { Navbar } from './components';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className='app'>
+      <Navbar />
+      <Header />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/container/Testimonials/Testimonials.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Testimonials.scss';
+
+function Testimonials() {
+  return <div>Testimonials</div>;
+}
+
+export default Testimonials;
