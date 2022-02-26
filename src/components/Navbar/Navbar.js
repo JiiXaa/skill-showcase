233e60b736cc3def928518a9134648eac02e7511
JiiXaa <anythingfx@gmail.com>
2022-02-26 21:31:55 +0100

    added hamburger menu for screens below 880px

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import './Navbar.scss';
+import React, { useState } from 'react';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 
+import './Navbar.scss';
 import { images } from '../../constants';
 
-const navItems = ['home', 'about', 'contact', 'projects', 'skills', 'contact'];
+const navItems = ['home', 'about', 'projects', 'skills', 'contact'];
 
 function Navbar() {
+  const [toggleHamburger, setToggleHamburger] = useState(false);
+
   return (
     <nav className='app__navbar'>
       <div className='app__navbar-logo-wrapper'>
@@ -21,6 +25,33 @@ function Navbar() {
           );
         })}
       </ul>
+      <div className='app__navbar-menu'>
+        <HiMenuAlt4 onClick={() => setToggleHamburger(true)} />
+
+        {toggleHamburger && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ x: [300, 0], opacity: 1 }}
+            transition={{ duration: 0.8, ease: 'easeOut' }}
+          >
+            <HiX onClick={() => setToggleHamburger(false)} />
+            <ul>
+              {navItems.map((item) => {
+                return (
+                  <li key={item}>
+                    <a
+                      href={`#${item}`}
+                      onClick={() => setToggleHamburger(false)}
+                    >
+                      {item}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </motion.div>
+        )}
+      </div>
     </nav>
   );
 }
