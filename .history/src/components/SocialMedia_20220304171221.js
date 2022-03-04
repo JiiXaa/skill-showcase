b2deb0ed509d81729958bcc0bb3dce2ec5bbb4d0
JiiXaa <anythingfx@gmail.com>
2022-03-04 18:38:18 +0000

    fixed issue with history folder

--- .history/src/components/SocialMedia_20220304171221.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { BsGithub, BsFacebook } from 'react-icons/bs';
-
-function SocialMedia() {
-  return <div>SocialMedia</div>;
-}
-
-export default SocialMedia;
--- .history/src/constants/items_20220304172313.js
+++
@@ -1,8 +0,0 @@
-export const navItems = [
-  'home',
-  'about',
-  'projects',
-  'skills',
-  'testimonials',
-  'contact',
-];
