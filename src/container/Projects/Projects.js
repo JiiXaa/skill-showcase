f53a6ed990b1383d05c86213cdae2e711b1765bd
JiiXaa <anythingfx@gmail.com>
2022-03-06 19:53:16 +0000

    added portfolio projects section

--- src/container/Projects/Projects.js
+++ src/container/Projects/Projects.js
@@ -1,8 +1,115 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
+
+import { AppWrapper } from '../../wrapper';
+import { urlFor, client } from '../../client';
+
 import './Projects.scss';
 
+const filterItems = ['JavaScript', 'ReactJS', 'NodeJS', 'All'];
+
+const handleProjectsFilter = (item) => {};
+
 function Projects() {
-  return <div>Projects</div>;
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+  const [projects, setProjects] = useState([]);
+  const [filterProjects, setFilterProjects] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "projects"]';
+
+    client.fetch(query).then((data) => {
+      setProjects(data);
+      setFilterProjects(data);
+    });
+  }, []);
+
+  return (
+    <>
+      <h2 className='head-text'>
+        My Portfolio
+        <span> Projects </span>
+        Expirience
+      </h2>
+
+      <div className='app__projects-filter'>
+        {filterItems.map((item, i) => (
+          <div
+            key={i}
+            onClick={() => handleProjectsFilter(item)}
+            className={`app__projects-filter-item app__flex p-text ${
+              activeFilter === item ? 'item-active' : ''
+            }`}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className='app__projects-portfolio'
+      >
+        {filterProjects.map((project, i) => (
+          <div className='app__projects-item app__flex' key={i}>
+            <div className='app__projects-img app_flex'>
+              <img src={urlFor(project.imgUrl)} alt={project.name} />
+
+              <motion.div
+                whileHover={{ opacity: [0, 1] }}
+                transition={{
+                  duration: 0.25,
+                  ease: 'easeInOut',
+                  staggerChildren: 0.4,
+                }}
+                className='app__projects-hover app__flex'
+              >
+                <a href={project.projectLink} target='_blank' rel='noreferrer'>
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{
+                      duration: 0.25,
+                    }}
+                    className='app__flex'
+                  >
+                    <AiFillEye />
+                  </motion.div>
+                </a>
+
+                <a href={project.codeLink} target='_blank' rel='noreferrer'>
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{
+                      duration: 0.25,
+                    }}
+                    className='app__flex'
+                  >
+                    <AiFillGithub />
+                  </motion.div>
+                </a>
+              </motion.div>
+            </div>
+
+            <div className='app__projects-content app__flex'>
+              <h4 className='bold-text'>{project.title}</h4>
+              <p className='p-text' style={{ marginTop: 10 }}>
+                {project.description}
+              </p>
+
+              <div className='app__projects-tag app__flex'>
+                <p className='p-text'>{project.tags[0]}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </motion.div>
+    </>
+  );
 }
 
-export default Projects;
+export default AppWrapper(Projects, 'projects');
